3cb70135751b17029be515e801379d4a77dbc9f0
Miller <millergarciaq@gmail.com>
2017-09-16 17:46:31 -0500

    landin page website

--- ecomerce_bike/ecomerce_bike/settings.py
+++ ecomerce_bike/ecomerce_bike/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.website',
 ]
 
 MIDDLEWARE = [
@@ -122,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_URL = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = ('static', )
 STATIC_ROOT = '/static/'
