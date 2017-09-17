edf8d87e94515eb33547b46fd7fdb7877ca4bd85
Miller <millergarciaq@gmail.com>
2017-09-17 10:16:53 -0500

    set models in admin site

--- ecomerce_bike/app/website/admin.py
+++ ecomerce_bike/app/website/admin.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import *
 from django.contrib import admin
 
 # Register your models here.
+class BikeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'motor', 'cylinder', 'photo','category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subjet', 'bike')
+
+admin.site.register(Bike, BikeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Contact, ContactAdmin)
